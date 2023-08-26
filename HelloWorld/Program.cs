8e0a0b3ea383a917418880266e88003997fedc89
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2023-08-26 10:32:05 -0500

    Change hello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace HelloWorld{
+    class Program{
+       
+        static void Main(){
+            string Name="Jhon";
+            Name="1Jhon";
+            Console.WriteLine($"Hello world! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
