f032b68ba316694cbb8c8718c1b50db044df2350
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2023-08-26 10:33:35 -0500

    Removing unused using

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,8 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-namespace HelloWorld{
+﻿namespace HelloWorld{
     class Program{
        
         static void Main(){
-            string Name="Jhon";
+            var Name="Jhon";
             Name="1Jhon";
             Console.WriteLine($"Hello world! {Name}");
             Console.WriteLine();
